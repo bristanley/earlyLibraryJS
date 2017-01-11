4189478735d02802d96485ec1039f92f91e841e3
Brian Stanley <Brian Stanley>
2017-01-11 11:14:15 -0500

    Java intergers and variables

---
+++ 0.01_intergers.js
@@ -0,0 +1,23 @@
+
+console.log("Hello World")
+console.log(1 + 1)
+console.log(1.2)
+console.log(5 +5 )
+console.log(6 - 3)
+console.log(12 / 4)
+console.log(17 % 5)
+
+
+console.log(27)
+console.log(1989)
+console.log(2012)
+console.log(1)
+
+console.log(10 % 3)
+console.log(17 % 5)
+console.log(62 % 25)
+//exponent taking number on left to the power of number on right. e takes it to the tenth power. Math.pow takes it as the given numbers//
+console.log(3e1)//3 x 10
+console.log(3e2)//3 x 10 x 10
+console.log(Math.pow(10,2))
+
---
+++ 0.02_variables.js
@@ -0,0 +1,57 @@
+//the box labeled christmas tree should contain a christmas tree. ain't nobody got time for racoons.
+//variables with strings
+/*
+var firstname = "brian"
+var lastname = "Stanley"
+var fullname = firstname + " " + lastname
+console.log(fullname)
+//Grammy will break everything that uses the interwebs. program with grammy in mind.
+//everything uses sequencing, computers are reading top to bottom, left to right.
+
+//empty variables
+var num
+console.log(num)
+
+//empty strings
+
+var petName = ""
+
+petName = "gus"
+
+console.log(petName)
+//let is new way of naming variables, allows for variables to be more specific
+let
+if (true){
+	let petName = ""
+}
+*/
+
+//shorthand operators
+//var age = 55
+//console.log(1 += 1) //1 = 1 + 1 **will not work
+//console.log(age += 1) //age = age + 1
+//console.log(age /= 2)
+//console.log(age *= 2)
+
+//var 2nite = "Wednesday Night" **No bueno
+
+//Challenge 
+
+var favoriteSportsTeam = "Reds"
+var favoriteTvShow = "Game of Thrones"
+var favoriteRestuarant = "Twenty Tap"
+var favoriteGame = "Civilization"
+var myName = "Brian"
+
+var myNumber = 0
+console.log(myNumber += 10)
+console.log(myNumber -= 1)
+console.log(myNumber *= 9)
+console.log(myNumber /= 7)
+
+var myAge = 27
+var maxAge = 120
+var glassesOfWaterPerDay = 3
+var totalNeeded = (glassesOfWaterPerDay * 365) * (maxAge - myAge)
+var message = "You will need" + " " + totalNeeded + " " + "glasses to last you until the ripe old age of" + maxAge;
+console.log(message)
